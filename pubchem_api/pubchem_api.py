b5261d7bc1ce6ba480ab09432d4f41c38516537c
simonholmes001 <simonholmesabc@gmail.com>
2020-02-22 11:39:01 +0100

    main API call module

--- pubchem_api/pubchem_api.py
+++ pubchem_api/pubchem_api.py
@@ -1,1 +1,19 @@
 """Main module."""
+import requests
+
+class api_get_features:
+
+    def __init__(self, base, input_1, i, input_2, j, output_format):
+        self.base = base
+        self.input_1 = input_1
+        self.input_2 = input_2
+        self.i = i
+        self.input_2 = input_2
+        self.j = j
+        self.output_format = output_format
+
+        request_api_data = requests.get(self.base+self.input_1+self.i+self.input_2+self.j+self.output_format)
+        print("The HTTP status code for this request is: {}. Well done Holmes, you've outdone yourself yet again!!!".format(request_api_data.status_code))
+
+        with open("./data/amino_acid_features.csv", "wb") as f:
+            f.write(request_api_data.content)
