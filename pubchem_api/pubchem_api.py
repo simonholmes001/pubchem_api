a586d5edbd17ff7fefc830f773d025783f1b9b31
simonholmes001 <simonholmesabc@gmail.com>
2020-02-25 16:16:27 +0100

    Improved code base for API call, standard name for class and explicit variable naming. Added in some lines of code to help the unit tests

--- pubchem_api/pubchem_api.py
+++ pubchem_api/pubchem_api.py
@@ -1,19 +1,24 @@
 """Main module."""
 import requests
 
-class api_get_features:
+class ApiGetFeatures:
 
-    def __init__(self, base, input_1, i, input_2, j, output_format):
-        self.base = base
-        self.input_1 = input_1
-        self.input_2 = input_2
-        self.i = i
-        self.input_2 = input_2
-        self.j = j
+    def __init__(self, base_url, compound_cid_selector, search_id, property, output_property, output_format, output_file_name):
+        self.base_url = base_url
+        self.compound_cid_selector = compound_cid_selector
+        self.search_id = search_id
+        self.property = property
+        self.output_property = output_property
         self.output_format = output_format
+        self.output_file_name = output_file_name
 
-        request_api_data = requests.get(self.base+self.input_1+self.i+self.input_2+self.j+self.output_format)
+        request_api_data = requests.get(self.base_url + self.compound_cid_selector + self.search_id + self.property + self.output_property + self.output_format)
+
         print("The HTTP status code for this request is: {}.".format(request_api_data.status_code))
 
-        with open("./data/amino_acid_features.csv", "wb") as f:
+        with open(output_file_name+".txt", "w") as f:
+           f.write("status code is {}.".format(str(request_api_data.status_code)))
+
+        with open(output_file_name+".csv", "wb") as f:
             f.write(request_api_data.content)
+
